2d409aa47178b2449a36fe38a0bffce6272fb9e7
chaeljin <86591127+chaeljin@users.noreply.github.com>
2021-08-25 01:20:42 +0900

    Upload EndView, Controller
    글자 깨진 것 확인. 오전 중 수정 예정

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,46 @@
 package controller;
 
+import model.dao.ReservationDAO;
+import model.dto.Attraction;
+import model.dto.Reservation;
+import view.EndView;
+
 public class Controller {
 
+	// 새로운 예약 저장
+	public void insertReservation(Reservation reservation) {
+		try {
+			ReservationDAO.addReservation(reservation);
+		} catch (Exception e) {
+			EndView.showError("새로운 예약 저장 오류 발생");
+		}
+	}
+
+	// 모든 예약 조회
+	public void getAllReservation() {
+		try {
+			EndView.showResListView(ReservationDAO.getAllReservations());
+		} catch (Exception e) {
+			EndView.showError("모든 예약 조회 오류 발생");
+		}
+	}
+
+	// 하나의 예약 조회
+	public void getOneReservation(int reservationId) {
+		try {
+			EndView.allView(ReservationDAO.getReservation(reservationId));
+		} catch (Exception e) {
+			EndView.showError("정보 검색 오류 발생");
+		}
+	}
+
+	public void updateReservation(int reservationId, Attraction attraction) {
+		try {
+			ReservationDAO.updateReservation(reservationId, attraction);
+
+		} catch (Exception e) {
+			EndView.showError("깨버리고싶어어");
+
+		}
+	}
 }
